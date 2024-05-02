646c10ab6a5cd31d9ffdce697c3642943b7cf77e
Maxime-Gld <Maxime.Gdev@hotmail.com>
2024-05-02 16:21:12 +0200

    regroupement des services dans PersonService

---
+++ src/main/java/com/safetynet/alertsapi/service/PersonService.java
@@ -0,0 +1,45 @@
+package com.safetynet.alertsapi.service;
+
+import java.util.List;
+
+import com.safetynet.alertsapi.model.HouseholdInformations;
+import com.safetynet.alertsapi.model.Person;
+
+public class PersonService {
+
+    public List<Person> getChildsByAddress(String address) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getChildsByAddress'");
+    }
+
+    public List<String> getPhonesByStationNumber(String stationNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPhonesByStationNumber'");
+    }
+
+    public List<Person> getPersonsByAdress(String address) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPersonsByAdress'");
+    }
+
+    public List<Person> getPersonsByStationNumber(String stationNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPersonsByStationNumber'");
+    }
+
+    public List<HouseholdInformations> getHouseholdInformationsByStations(List<Integer> stations) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHouseholdInformationsByStations'");
+    }
+
+    public List<String> getAllEmailsByCity(String city) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllEmailsByCity'");
+    }
+
+    public Person getPersonInfoByName(String firstName, String lastName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPersonInfoByName'");
+    }
+
+}
