eafb18bdc50519bfae654da56517dfa3d9c5e7fb
Max-Dev <Maxime.Gdev@hotmail.com>
2024-05-16 23:27:49 +0200

    suppr du test par defaut

--- src/test/java/com/safetynet/alertsapi/AlertsApiApplicationTests.java
+++ src/test/java/com/safetynet/alertsapi/AlertsApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.safetynet.alertsapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlertsApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
