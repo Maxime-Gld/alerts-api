d81aeb0756a6bdbdfef5de02f99889427e964c5c
Max-Dev <Maxime.Gdev@hotmail.com>
2024-05-27 07:14:27 +0200

    a refaire

--- src/test/java/com/safetynet/alertsapi/repository/FirestationRepositoryTest.java
+++ src/test/java/com/safetynet/alertsapi/repository/FirestationRepositoryTest.java
@@ -1,132 +1,134 @@
-package com.safetynet.alertsapi.repository;
+// TODO :  refaire les tests 
 
-import static org.junit.jupiter.api.Assertions.*;
+// package com.safetynet.alertsapi.repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+// import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.List;
 
-import com.safetynet.alertsapi.model.Firestation;
-import com.safetynet.alertsapi.repository.implementation.FirestationRepositoryImpl;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
 
-public class FirestationRepositoryTest {
+// import com.safetynet.alertsapi.model.Firestation;
+// import com.safetynet.alertsapi.repository.implementation.FirestationRepositoryImpl;
+
+// public class FirestationRepositoryTest {
     
-    private FirestationRepository firestationRepository;
+//     private FirestationRepository firestationRepository;
     
-    @BeforeEach
-    public void setUp() {
-        List<Firestation> firestations = new ArrayList<>(Arrays.asList(
-            new Firestation("123 Main St", "1"),
-            new Firestation("456 Elm St", "2"),
-            new Firestation("789 Oak St", "3")
-        ));
-        firestationRepository = new FirestationRepositoryImpl(firestations);
-    }
+//     @BeforeEach
+//     public void setUp() {
+//         List<Firestation> firestations = new ArrayList<>(Arrays.asList(
+//             new Firestation("123 Main St", "1"),
+//             new Firestation("456 Elm St", "2"),
+//             new Firestation("789 Oak St", "3")
+//         ));
+//         firestationRepository = new FirestationRepositoryImpl(firestations);
+//     }
 
-    @Test
-    public void testFindAll() {
-        // Given (setUp)
+//     @Test
+//     public void testFindAll() {
+//         // Given (setUp)
 
-        // When
-        List<Firestation> firestations = firestationRepository.findAll();
+//         // When
+//         List<Firestation> firestations = firestationRepository.findAll();
 
-        // Then
-        assertNotNull(firestations);
-        assertFalse(firestations.isEmpty()) ;      
-    }
+//         // Then
+//         assertNotNull(firestations);
+//         assertFalse(firestations.isEmpty()) ;      
+//     }
 
-    @Test
-    public void testFindByAddress() {
-        // Given
-        String address = "123 Main St";
+//     @Test
+//     public void testFindByAddress() {
+//         // Given
+//         String address = "123 Main St";
 
-        // When
-        Firestation foundFirestation = firestationRepository.findByAddress(address);
+//         // When
+//         Firestation foundFirestation = firestationRepository.findByAddress(address);
 
-        // Then
-        assertNotNull(foundFirestation);
-        assertEquals(address, foundFirestation.getAddress());
-    }
+//         // Then
+//         assertNotNull(foundFirestation);
+//         assertEquals(address, foundFirestation.getAddress());
+//     }
 
-    @Test
-    public void testFindByAddressNotFound() {
-        // Given
-        String address = "999 Main St";
+//     @Test
+//     public void testFindByAddressNotFound() {
+//         // Given
+//         String address = "999 Main St";
 
-        // When
-        Firestation foundFirestation = firestationRepository.findByAddress(address);
+//         // When
+//         Firestation foundFirestation = firestationRepository.findByAddress(address);
 
-        // Then
-        assertNull(foundFirestation);
-    }
+//         // Then
+//         assertNull(foundFirestation);
+//     }
 
-    @Test
-    public void testFindByStationNumber() {
-        // Given
-        String stationNumber = "1";
+//     @Test
+//     public void testFindByStationNumber() {
+//         // Given
+//         String stationNumber = "1";
 
-        // When
-        Firestation foundFirestation = firestationRepository.findByStationNumber(stationNumber);
+//         // When
+//         Firestation foundFirestation = firestationRepository.findByStationNumber(stationNumber);
 
-        // Then
-        assertNotNull(foundFirestation);
-        assertEquals(stationNumber, foundFirestation.getStation());
-    }
+//         // Then
+//         assertNotNull(foundFirestation);
+//         assertEquals(stationNumber, foundFirestation.getStation());
+//     }
 
-    @Test
-    public void testFindByStationNumberNotFound() {
-        // Given
-        String stationNumber = "4";
+//     @Test
+//     public void testFindByStationNumberNotFound() {
+//         // Given
+//         String stationNumber = "4";
 
-        // When
-        Firestation foundFirestation = firestationRepository.findByStationNumber(stationNumber);
+//         // When
+//         Firestation foundFirestation = firestationRepository.findByStationNumber(stationNumber);
 
-        // Then
-        assertNull(foundFirestation);
-    }
+//         // Then
+//         assertNull(foundFirestation);
+//     }
 
-    @Test
-    public void testSave() {
-        // Given
-        Firestation newFirestation = new Firestation("999 Main St", "4");
+//     @Test
+//     public void testSave() {
+//         // Given
+//         Firestation newFirestation = new Firestation("999 Main St", "4");
         
-        // When
-        firestationRepository.save(newFirestation);
+//         // When
+//         firestationRepository.save(newFirestation);
         
-        List<Firestation> firestations = firestationRepository.findAll();
-        // Then
-        assertTrue(firestations.contains(newFirestation));
-    }
+//         List<Firestation> firestations = firestationRepository.findAll();
+//         // Then
+//         assertTrue(firestations.contains(newFirestation));
+//     }
 
-    @Test
-    public void testUpdate() {
-        // Given
-        List<Firestation> firestations = firestationRepository.findAll();
-        Firestation firestationBeforeUpdate = firestations.get(0); // station 1
-        Firestation updateFirestation = new Firestation("540 Main St", "1");
+//     @Test
+//     public void testUpdate() {
+//         // Given
+//         List<Firestation> firestations = firestationRepository.findAll();
+//         Firestation firestationBeforeUpdate = firestations.get(0); // station 1
+//         Firestation updateFirestation = new Firestation("540 Main St", "1");
 
-        // When
-        firestationRepository.update(updateFirestation);
+//         // When
+//         firestationRepository.update(updateFirestation);
 
-        // Then
-        assertFalse(firestations.contains(firestationBeforeUpdate));
-        assertTrue(firestations.contains(updateFirestation));
-    }
+//         // Then
+//         assertFalse(firestations.contains(firestationBeforeUpdate));
+//         assertTrue(firestations.contains(updateFirestation));
+//     }
 
-    @Test
-    public void testDelete() {
-        // Given
-        List<Firestation> firestations = firestationRepository.findAll();
-        Firestation firestationToDelete = firestations.get(0); // station 1
+//     @Test
+//     public void testDelete() {
+//         // Given
+//         List<Firestation> firestations = firestationRepository.findAll();
+//         Firestation firestationToDelete = firestations.get(0); // station 1
 
-        // When
-        firestationRepository.delete(firestationToDelete);
+//         // When
+//         firestationRepository.delete(firestationToDelete);
 
 
-        // Then
-        assertFalse(firestations.contains(firestationToDelete));
-    }
-}
+//         // Then
+//         assertFalse(firestations.contains(firestationToDelete));
+//     }
+// }
