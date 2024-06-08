9544064c886a38b63f160049e190c2248545ac40
Max-Dev <Maxime.Gdev@hotmail.com>
2024-06-08 17:02:08 +0200

    passage du DtoMapper en component non statique

--- src/main/java/com/safetynet/alertsapi/utils/DtoMapper.java
+++ src/main/java/com/safetynet/alertsapi/utils/DtoMapper.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.safetynet.alertsapi.dto.ResponseEmailDTO;
 import com.safetynet.alertsapi.dto.ResponseFloodDTO;
 import com.safetynet.alertsapi.dto.ResponsePhoneAlertDTO;
@@ -14,139 +17,121 @@ import com.safetynet.alertsapi.dto.persondto.PersonResponseFireDTO;
 import com.safetynet.alertsapi.dto.persondto.PersonResponseFirestationDTO;
 import com.safetynet.alertsapi.dto.persondto.PersonResponseFloodDTO;
 import com.safetynet.alertsapi.model.Person;
-import com.safetynet.alertsapi.repository.MedicalRecordRepository;
 import com.safetynet.alertsapi.service.PersonService;
 
+@Component
 public class DtoMapper {
-    
-    private static PersonService personService;
-    private static MedicalRecordRepository medicalRecordRepository;
-    
-    public static PersonResponseFirestationDTO toPersonResponseFirestationDTO(Person person) {
-        return new PersonResponseFirestationDTO(
-                person.getFirstName(),
-                person.getLastName(),
-                person.getAddress(),
-                person.getPhone()
-        );
-    }
 
-    public static List<PersonResponseFirestationDTO> toPersonResponseFirestationDTOList(List<Person> persons) {
-        return persons.stream()
-            .map(DtoMapper::toPersonResponseFirestationDTO)
-            .toList();
-    }
+        private PersonService personService;
 
-    public static PersonResponseChildAlertDTO toPersonResponseChildAlertDTO(Person person) {
-        return new PersonResponseChildAlertDTO(
-                person.getFirstName(),
-                person.getLastName(),
-                personService.getAge(medicalRecordRepository.findByFirstnameAndLastname(
-                        person.getFirstName(),
-                        person.getLastName()
-                ))
-        );
-    }
+        @Autowired
+        public DtoMapper(PersonService personService) {
+                this.personService = personService;
+        }
 
-    public static List<PersonResponseChildAlertDTO> toPersonResponseChildAlertDTOList(List<Person> persons) {
-        return persons.stream()
-            .map(DtoMapper::toPersonResponseChildAlertDTO)
-            .toList();
-    }
+        public PersonResponseFirestationDTO toPersonResponseFirestationDTO(Person person) {
+                return new PersonResponseFirestationDTO(
+                                person.getFirstName(),
+                                person.getLastName(),
+                                person.getAddress(),
+                                person.getPhone());
+        }
 
-    public static ResponseEmailDTO toResponseEmailDTO(List<Person> personsByCity) {
-        return new ResponseEmailDTO(personsByCity.stream()
-                .map(p -> p.getEmail())
-                .toList());
-    }
+        public List<PersonResponseFirestationDTO> toPersonResponseFirestationDTOList(List<Person> persons) {
+                return persons.stream()
+                                .map(this::toPersonResponseFirestationDTO)
+                                .toList();
+        }
 
-    public static ResponsePhoneAlertDTO toResponsePhoneAlertDTO(List<Person> personsByStationNumber) {
-        return new ResponsePhoneAlertDTO(personsByStationNumber.stream()
-                .map(p -> p.getPhone())
-                .toList());
-    }
+        public PersonResponseChildAlertDTO toPersonResponseChildAlertDTO(Person person) {
+                return new PersonResponseChildAlertDTO(
+                                person.getFirstName(),
+                                person.getLastName(),
+                                personService.getAge(person));
+        }
 
-    public static PersonResponseFireDTO toPersonResponseFireDTO(Person person) {
-        return new PersonResponseFireDTO(
-                person.getFirstName(),
-                person.getLastName(),
-                person.getPhone(),
-                personService.getAge(medicalRecordRepository.findByFirstnameAndLastname(
-                        person.getFirstName(),
-                        person.getLastName()
-                )),
-                toMedicalRecordBaseDTO(person)
-        );
-    }
+        public List<PersonResponseChildAlertDTO> toPersonResponseChildAlertDTOList(List<Person> persons) {
+                return persons.stream()
+                                .map(this::toPersonResponseChildAlertDTO)
+                                .toList();
+        }
 
-    public static MedicalRecordBaseDTO toMedicalRecordBaseDTO(Person person) {
-        return new MedicalRecordBaseDTO(
-                medicalRecordRepository.findByFirstnameAndLastname(
-                        person.getFirstName(),
-                        person.getLastName()
-                ).getMedications(),
-                medicalRecordRepository.findByFirstnameAndLastname(
-                        person.getFirstName(),
-                        person.getLastName()
-                ).getAllergies());
-    }
+        public ResponseEmailDTO toResponseEmailDTO(List<Person> personsByCity) {
+                return new ResponseEmailDTO(personsByCity.stream()
+                                .map(p -> p.getEmail())
+                                .toList());
+        }
 
-    public static List<PersonResponseFireDTO> toPersonResponseFireDTOList(List<Person> persons) {
-        return persons.stream()
-            .map(DtoMapper::toPersonResponseFireDTO)
-            .toList();
-    }
+        public ResponsePhoneAlertDTO toResponsePhoneAlertDTO(List<Person> personsByStationNumber) {
+                return new ResponsePhoneAlertDTO(personsByStationNumber.stream()
+                                .map(p -> p.getPhone())
+                                .toList());
+        }
 
-    public static PersonResponseFloodDTO toPersonResponseFloodDTO(Person person) {
-        return new PersonResponseFloodDTO(
-                person.getFirstName(),
-                person.getLastName(),
-                person.getPhone(),
-                personService.getAge(medicalRecordRepository.findByFirstnameAndLastname(
-                        person.getFirstName(),
-                        person.getLastName()
-                )),
-                toMedicalRecordBaseDTO(person)
-        );
-    }
+        public PersonResponseFireDTO toPersonResponseFireDTO(Person person) {
+                return new PersonResponseFireDTO(
+                                person.getFirstName(),
+                                person.getLastName(),
+                                person.getPhone(),
+                                personService.getAge(person),
+                                toMedicalRecordBaseDTO(person));
+        }
 
-    public static List<PersonResponseFloodDTO> toPersonResponseFloodDTOList(List<Person> persons) {
-        return persons.stream()
-            .map(DtoMapper::toPersonResponseFloodDTO)
-            .toList();
-    }
+        public MedicalRecordBaseDTO toMedicalRecordBaseDTO(Person person) {
+                return new MedicalRecordBaseDTO(
+                                personService.getMedicalRecord(person).getMedications(),
+                                personService.getMedicalRecord(person).getAllergies());
+        }
 
-    public static Map<String, List<PersonResponseFloodDTO>> toMapPersonResponseFloodDTOList(List<Person> persons) {
-        return persons.stream()
-            .collect(Collectors.groupingBy(
-                Person::getAddress,
-                Collectors.mapping(DtoMapper::toPersonResponseFloodDTO, Collectors.toList())
-            ));
-    }
+        public List<PersonResponseFireDTO> toPersonResponseFireDTOList(List<Person> persons) {
+                return persons.stream()
+                                .map(this::toPersonResponseFireDTO)
+                                .toList();
+        }
 
-    public static List<ResponseFloodDTO> toResponseFloodDTOList(Map<String, List<PersonResponseFloodDTO>> personsByAddress) {
-        return personsByAddress.entrySet().stream()
-            .map(entry -> new ResponseFloodDTO(entry.getKey(), entry.getValue()))
-            .toList();
-    }
+        public PersonResponseFloodDTO toPersonResponseFloodDTO(Person person) {
+                return new PersonResponseFloodDTO(
+                                person.getFirstName(),
+                                person.getLastName(),
+                                person.getPhone(),
+                                personService.getAge(person),
+                                toMedicalRecordBaseDTO(person));
+        }
 
-    public static PersonInfoDTO toPersonInfoDTO(Person person) {
-        return new PersonInfoDTO(
-                person.getFirstName(),
-                person.getLastName(),
-                person.getAddress(),
-                personService.getAge(medicalRecordRepository.findByFirstnameAndLastname(
-                        person.getFirstName(),
-                        person.getLastName()
-                )),
-                person.getEmail(),
-                toMedicalRecordBaseDTO(person)
-        );
-    }
+        public List<PersonResponseFloodDTO> toPersonResponseFloodDTOList(List<Person> persons) {
+                return persons.stream()
+                                .map(this::toPersonResponseFloodDTO)
+                                .toList();
+        }
 
-    public static List<PersonInfoDTO> toPersonInfoDTOList(List<Person> persons) {
-        return persons.stream()
-            .map(DtoMapper::toPersonInfoDTO)
-            .toList();
-    }
+        public Map<String, List<PersonResponseFloodDTO>> toMapPersonResponseFloodDTOList(List<Person> persons) {
+                return persons.stream()
+                                .collect(Collectors.groupingBy(
+                                                Person::getAddress,
+                                                Collectors.mapping(this::toPersonResponseFloodDTO,
+                                                                Collectors.toList())));
+        }
+
+        public List<ResponseFloodDTO> toResponseFloodDTOList(
+                        Map<String, List<PersonResponseFloodDTO>> personsByAddress) {
+                return personsByAddress.entrySet().stream()
+                                .map(entry -> new ResponseFloodDTO(entry.getKey(), entry.getValue()))
+                                .toList();
+        }
+
+        public PersonInfoDTO toPersonInfoDTO(Person person) {
+                return new PersonInfoDTO(
+                                person.getFirstName(),
+                                person.getLastName(),
+                                person.getAddress(),
+                                personService.getAge(person),
+                                person.getEmail(),
+                                toMedicalRecordBaseDTO(person));
+        }
+
+        public List<PersonInfoDTO> toPersonInfoDTOList(List<Person> persons) {
+                return persons.stream()
+                                .map(this::toPersonInfoDTO)
+                                .toList();
+        }
 }
