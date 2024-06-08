8772d8695b8c4a71ae2fe2afe0cc22ff34ca2986
Max-Dev <Maxime.Gdev@hotmail.com>
2024-06-08 23:42:12 +0200

    ajout des constante pour Endpoints

---
+++ src/main/java/com/safetynet/alertsapi/config/constant/EndPointConstant.java
@@ -0,0 +1,17 @@
+package com.safetynet.alertsapi.config.constant;
+
+public class EndPointConstant {
+    
+    public static final String API = "/api/v1";
+    public static final String HEALTH = API + "/health";
+    public static final String FIRESTATION = "/firestation";
+    public static final String CHILD = "/childAlert";
+    public static final String PHONE = "/phoneAlert";
+    public static final String FIRE = "/fire";
+    public static final String FLOOD = "/flood/stations";
+    public static final String PERSON_INFO = "/personInfolastName";
+    public static final String EMAIL = "/communityEmail";
+    public static final String PERSON = "/person";
+    public static final String MEDICAL_RECORD = "/medicalRecord";
+
+}
